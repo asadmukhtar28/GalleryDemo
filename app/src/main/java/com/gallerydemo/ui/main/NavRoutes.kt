7afa8f0c1c63a7d96777acf1d0802639c0bdc117
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-06-24 13:39:26 +0500

    Define NavRoutes.kt object that contains navigation routes.

---
+++ app/src/main/java/com/gallerydemo/ui/main/NavRoutes.kt
@@ -0,0 +1,7 @@
+package com.gallerydemo.ui.main
+
+object NavRoutes {
+    const val PERMISSION_SCREEN = "PERMISSION_SCREEN"
+    const val FOLDERS_SCREEN = "FOLDERS_SCREEN"
+    const val MEDIA_SCREEN = "MEDIA_SCREEN"
+}
