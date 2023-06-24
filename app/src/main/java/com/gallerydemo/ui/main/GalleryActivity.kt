07c2dfcc56f5654a513ec1404051377fb7f42be2
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-06-24 13:44:01 +0500

    Define splash screen theme at themes.xml and integrate the splash api at GalleryActivity.kt.

--- app/src/main/java/com/gallerydemo/ui/main/GalleryActivity.kt
+++ app/src/main/java/com/gallerydemo/ui/main/GalleryActivity.kt
@@ -3,23 +3,32 @@ package com.gallerydemo.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gallerydemo.ui.theme.GalleryDemoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryActivity : ComponentActivity() {
+    val viewModel: SharedViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             GalleryDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {}
+                ) {
+                }
             }
         }
     }
+
 }
