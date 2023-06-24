515cf8c41dc59a075848022b0065abcea41e0b98
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-06-25 01:44:22 +0500

    Fix the issue of gallery folder title null by defining the title to be nullable at GalleryFolder.kt.

--- app/src/main/java/com/gallerydemo/data/local/models/GalleryFolder.kt
+++ app/src/main/java/com/gallerydemo/data/local/models/GalleryFolder.kt
@@ -1,11 +1,3 @@
 package com.gallerydemo.data.local.models
 
-data class GalleryFolder(val title: String, val mediaList: MutableList<MediaItem>) {/*
-        override fun equals(other: Any?): Boolean {
-            return other is GalleryFolder && other.title == title
-        }
-
-        override fun hashCode(): Int {
-            return super.hashCode()
-        }*/
-}
+data class GalleryFolder(val title: String? = "", val mediaList: MutableList<MediaItem>)
