5ae602dac3617c3d4d3b2d3a3f7f992a3486025f
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-06-25 15:47:08 +0500

    Implementation of fetching Media from mediaStore at GalleryActivityViewModel.kt and also write the logic to do not load the data again onConfiguration changes.

---
+++ app/src/main/java/com/gallerydemo/ui/main/GalleryActivityViewModel.kt
@@ -0,0 +1,68 @@
+package com.gallerydemo.ui.main
+
+import android.content.ContentResolver
+import androidx.lifecycle.viewModelScope
+import com.gallerydemo.data.local.models.GalleryFolder
+import com.gallerydemo.data.repository.GalleryRepositoryImpl
+import com.gallerydemo.ui.base.BaseViewModel
+import com.gallerydemo.ui.main.folder.GalleryFolderUiState
+import com.gallerydemo.utils.State
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GalleryActivityViewModel @Inject constructor(private val repository: GalleryRepositoryImpl) :
+    BaseViewModel() {
+
+    private val _galleryUiState = MutableStateFlow(GalleryFolderUiState(isLoading = true))
+    val galleryUiState: StateFlow<GalleryFolderUiState> = _galleryUiState.asStateFlow()
+
+    private val _selectedItemPosition = MutableStateFlow(GalleryFolder())
+    val selectedItemPosition: StateFlow<GalleryFolder> = _selectedItemPosition
+
+    fun fetchGallery(contentResolver: ContentResolver, stringProvider: (resId: Int) -> String) {
+        /*
+        * As on Configuration change fetchGallery() again called so before loading the data
+        * again, just check if data is loaded then return
+        */
+        if (galleryUiState.value.galleryFolderList.isNotEmpty())
+            return
+        else
+            viewModelScope.launch(Dispatchers.IO) {
+                repository.loadMediaFromStorage(
+                    contentResolver,
+                    stringProvider = { resId -> stringProvider.invoke(resId) })
+                    .collectLatest { response ->
+                        when (response) {
+                            is State.Failure -> {
+                                _galleryUiState.value = _galleryUiState.value.copy(
+                                    isLoading = false,
+                                    galleryFolderList = emptyList()
+                                )
+                            }
+
+                            State.Loading -> {
+                                _galleryUiState.value = _galleryUiState.value.copy(isLoading = true)
+                            }
+
+                            is State.Success -> {
+                                _galleryUiState.value = _galleryUiState.value.copy(
+                                    isLoading = false,
+                                    galleryFolderList = response.data
+                                )
+                            }
+                        }
+                    }
+            }
+    }
+
+    fun setSelectedGalleryFolderItem(folder: GalleryFolder) {
+        _selectedItemPosition.value = folder
+    }
+}
