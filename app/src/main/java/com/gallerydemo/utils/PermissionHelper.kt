61c3130562cfdfc7835a07f90172ceb417665c29
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-06-25 21:56:48 +0500

    Refactor PermissionHelper.kt, convert it to object for singleton instance.

--- app/src/main/java/com/gallerydemo/utils/PermissionHelper.kt
+++ app/src/main/java/com/gallerydemo/utils/PermissionHelper.kt
@@ -3,15 +3,18 @@ package com.gallerydemo.utils
 import android.Manifest
 import android.os.Build
 
+object PermissionHelper {
+    private fun isGranularPermissionsSupport() =
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
 
-private fun isGranularPermissionsSupport() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+    fun getPermissionList() = if (isGranularPermissionsSupport()) {
+        arrayOf(
+            Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
 
-fun getPermissionList() = if (isGranularPermissionsSupport()) {
-    arrayOf(
-        Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO
-    )
-} else {
-    arrayOf(
-        Manifest.permission.READ_EXTERNAL_STORAGE
-    )
 }
