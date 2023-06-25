3ef80463458dd4c2d9e58e488fecedb5a4b6c10f
Asad Mukhtar <asadmukhtar28@gmail.com>
2023-06-25 16:23:24 +0500

    Handle the logic of fetching the thumb if mediaType is video and some refactor code at MediaList.kt

--- app/src/main/java/com/gallerydemo/ui/main/media/MediaList.kt
+++ app/src/main/java/com/gallerydemo/ui/main/media/MediaList.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -19,32 +20,53 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.VideoFrameDecoder
+import coil.request.CachePolicy
 import com.gallerydemo.R
+import com.gallerydemo.data.local.models.GalleryFolder
+import com.gallerydemo.data.local.models.MediaItem
+import com.gallerydemo.ui.main.common.EmptyComponent
 import com.gallerydemo.ui.theme.GalleryDemoTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MediaListScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { MediaListToolbar() }) { innerPadding ->
-        LazyVerticalGrid(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            columns = GridCells.Fixed(integerResource(id = R.integer.media_grid_span_count))
-        ) {
-            items(20) {
-                ItemMediaView()
+fun MediaListScreen(galleryFolder: GalleryFolder = GalleryFolder()) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { MediaListToolbar(galleryFolder.title ?: "") }) { innerPadding ->
+
+        when {
+            galleryFolder.mediaList.size > 0 -> {
+                LazyVerticalGrid(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    columns = GridCells.Fixed(integerResource(id = R.integer.media_grid_span_count))
+                ) {
+                    items(items = galleryFolder.mediaList) { media ->
+                        ItemMediaView(media)
+                    }
+                }
             }
+
+            else -> EmptyComponent()
         }
     }
 }
@@ -74,32 +96,72 @@ private fun MediaListToolbar(title: String = "All Images") {
 }
 
 @Composable
-fun ItemMediaView() {
-    Box(modifier = Modifier.fillMaxWidth()) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_default_thumbnail),
-            contentDescription = stringResource(
-                id = R.string.thumbnail
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-        )
+fun ItemMediaView(media: MediaItem) {
 
-        Box(
-            modifier = Modifier
-                .size(dimensionResource(id = R.dimen.media_play_icon_size))
-                .align(Alignment.Center)
-                .clip(shape = CircleShape)
-                .background(color = Color.Gray)
-        ) {
-            Image(
-                modifier = Modifier.align(Alignment.Center),
-                painter = painterResource(id = R.drawable.ic_play),
-                contentDescription = stringResource(
-                    id = R.string.thumbnail
-                )
+    Box(modifier = Modifier.fillMaxWidth()) {
+        val context = LocalContext.current
+        if (media.isVideo) {
+            val imageLoader = remember {
+                ImageLoader.Builder(context)
+                    .memoryCachePolicy(CachePolicy.ENABLED)
+                    .diskCachePolicy(CachePolicy.ENABLED)
+                    .components { add(VideoFrameDecoder.Factory()) }.crossfade(true)
+                    .build()
+            }
+            val painter = rememberAsyncImagePainter(
+                model = media.mediaPath,
+                imageLoader = imageLoader,
             )
+
+            if (painter.state is AsyncImagePainter.State.Loading) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_default_thumbnail),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop,
+                )
+            }
+
+            Image(
+                painter = painter,
+                contentDescription = stringResource(id = R.string.thumbnail),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+            )
+
+        } else {
+            AsyncImage(
+                model = media.mediaPath,
+                contentDescription = stringResource(id = R.string.thumbnail),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.ic_default_thumbnail),
+                error = painterResource(id = R.drawable.ic_default_thumbnail)
+            )
+        }
+
+        if (media.isVideo) {
+            Box(
+                modifier = Modifier
+                    .size(dimensionResource(id = R.dimen.media_play_icon_size))
+                    .align(Alignment.Center)
+                    .clip(shape = CircleShape)
+                    .background(color = Color.Gray)
+            ) {
+                Image(
+                    modifier = Modifier.align(Alignment.Center),
+                    painter = painterResource(id = R.drawable.ic_play),
+                    contentDescription = stringResource(
+                        id = R.string.thumbnail
+                    )
+                )
+            }
         }
     }
 }
